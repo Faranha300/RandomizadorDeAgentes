48b67ebf48502c0e161d9aa07c745992c0f17d4d
Fabrício Aranha <fabricioaranha1@gmail.com>
2022-05-30 20:51:30 -0300

    Atualizando o nome do projeto

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/fabri/CLionProjects/EscolherBoneco
+# Install script for directory: C:/Users/fabri/CLionProjects/RandomizadorDeAgentes
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/fabri/CLionProjects/EscolherBoneco/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/fabri/CLionProjects/RandomizadorDeAgentes/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
